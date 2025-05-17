b83d8a31347740957b5bd0f6ce08395fafa49b1d
Sergey <sergey.krishnev.97@gmail.com>
2025-05-17 12:35:30 +0200

    fix ui after adding theory

--- src/api/skillApi.ts
+++ src/api/skillApi.ts
@@ -16,8 +16,8 @@ export const skillApi = createApi({
             async onQueryStarted({ id, professionId, ...rest }, { dispatch, queryFulfilled }) {
                 const patchResult = dispatch(
                     professionApi.util.updateQueryData('getSkillsByProfessionId', professionId, (draft) => {
-                        console.log(draft.map((skill: { id: number; }) => skill.id === id ? {...skill, ...rest} : skill))
-                        return draft.map((skill: { id: number; }) => skill.id === id ? {...skill, ...rest} : skill)
+                        console.log(draft.map((skill: { id: number; }) => skill.id === id ? { ...skill, ...rest } : skill))
+                        return draft.map((skill: { id: number; }) => skill.id === id ? { ...skill, ...rest } : skill)
                     }),
                 )
                 try {
@@ -34,12 +34,46 @@ export const skillApi = createApi({
             }),
         }),
         addNewTheoryToSkill: builder.mutation<void, { title: string; content: string; skillId?: number; parent?: UniqueIdentifier | null }>({
-            query: ({skillId, ...rest}) => ({
+            query: ({ skillId, ...rest }) => ({
                 url: `/${skillId}/theories`,
                 method: "POST",
                 body: rest,
             }),
-        }),
+            async onQueryStarted(_data, { dispatch, queryFulfilled }) {
+                const { data: theory } = await queryFulfilled
+
+                const addTheoryImmutable = (theories: any[], newTheory: any): any[] => {
+                    return theories.map(theory => {
+                        if (theory.id === newTheory.parent) {
+                            return {
+                                ...theory,
+                                subTheories: theory.subTheories
+                                    ? [...theory.subTheories, newTheory]
+                                    : [newTheory],
+                            };
+                        }
+                        if (theory.subTheories) {
+                            return {
+                                ...theory,
+                                subTheories: addTheoryImmutable(theory.subTheories, newTheory),
+                            };
+                        }
+                        return theory;
+                    })
+                }
+
+                const patchResult = dispatch(
+                        skillApi.util.updateQueryData('getTheoriesBySkillId', theory?.skill, (draft) => {
+                            return theory?.parent ? addTheoryImmutable(draft, theory) : [...draft, theory]
+                        }),
+                    )
+                    try {
+                        await queryFulfilled
+                    } catch {
+                        patchResult.undo()
+                    }
+                },
+            }),
         moveTheory: builder.mutation<void, { skillId: number; targetTheoryId: number; newParentId?: number | null; newIndexPosition: number }>({
             query: ({ skillId, targetTheoryId, newIndexPosition, newParentId }) => ({
                 url: `/${skillId}/theories/move-theory`,
@@ -47,7 +81,7 @@ export const skillApi = createApi({
                 params: {
                     targetTheoryId,
                     newIndexPosition,
-                    ...(newParentId ? {newParentId} : {}),
+                    ...(newParentId ? { newParentId } : {}),
                 },
             }),
         }),
